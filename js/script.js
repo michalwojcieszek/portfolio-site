397fdfbf1ebc23a96654f8ba941f9b4084175f08
michalwojcieszek <mwoj772@gmail.com>
2024-04-11 19:36:39 +0200

    after adding focus

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEl = document.querySelector("header");
+const linkEl = headerEl.querySelectorAll("a:link");
 
 btnNavEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
+});
+
+linkEl.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    headerEl.classList.toggle("nav-open");
+  });
 });
